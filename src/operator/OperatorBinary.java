a07907641c72ed5bf3a3bb6d90d1cfffa4674598
sitialiyahyah <sitialiyah94.net@gmail.com>
2023-06-15 10:19:10 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Siti Aliyah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
